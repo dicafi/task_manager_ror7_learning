1ab25f143f9c33154949461ac304a93c3a5197dd
Julio Acevedo <julio.acevedo@wizeline.com>
2024-07-04 12:21:23 -0600

    Change Yes/No status with simulated X and Check marks. Move common view code to helper. Remove not needed 'Back to Home' links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@
 
 # /app/helpers/application_helper.rb
 module ApplicationHelper
+  def error_messages_for(object)
+    render(partial: 'layouts/error_messages', locals: { object: object })
+  end
+
+  def status_tag(boolean)
+    if boolean
+      content_tag(:span, '', class: 'status true')
+    else
+      content_tag(:span, '', class: 'status false')
+    end
+  end
 end
